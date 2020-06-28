858bd9327aaa3c46d14c7d80f4ef109fa3340238
Константин <bluesbaker@mail.ru>
2020-06-29 00:55:09 +0300

    Add project files.

---
+++ IPScan/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPScan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Write("Press any key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
