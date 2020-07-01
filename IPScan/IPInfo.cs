74d6142072d2cc9e4deaace4ca2723315f9e1700
Константин <bluesbaker@mail.ru>
2020-07-01 17:54:20 +0300

    Add IPInfo

---
+++ IPScan/IPInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPScan
+{
+    public class IPInfo : Dictionary<string, string>
+    {
+
+    }
+}
