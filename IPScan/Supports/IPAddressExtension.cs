a8c21c9ab7610f2b1cf794b8bd4fcc8e343677f8
Константин <bluesbaker@mail.ru>
2020-07-01 17:53:36 +0300

    Add IPAddressExtension

---
+++ IPScan/Supports/IPAddressExtension.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPScan.Supports
+{
+    public static class IPAddressExtension
+    {
+        public static List<IPAddress> Range(this IPAddress startRange, IPAddress endRange)
+        {
+            var result = new List<IPAddress>();
+
+            // convert IPAdresses to bytes
+            var fromAsBytes = startRange.GetAddressBytes();
+            var toAsBytes = endRange.GetAddressBytes();
+
+            // reverse the from-to bytes 
+            // for a correct calculate of range
+            Array.Reverse(fromAsBytes);
+            Array.Reverse(toAsBytes);
+
+            // convert bytes to int
+            var fromAsInt = BitConverter.ToInt32(fromAsBytes, 0);
+            var toAsInt = BitConverter.ToInt32(toAsBytes, 0);
+
+            if (fromAsInt > toAsInt)
+            {
+                throw new Exception($"Input error: {startRange.ToString()} less {endRange.ToString()}");
+            }
+
+            // calculate the range
+            for (int ipAsInt = fromAsInt; ipAsInt <= toAsInt; ipAsInt++)
+            {
+                var ipAsBytes = BitConverter.GetBytes(ipAsInt);
+                // more reverse... more!
+                Array.Reverse(ipAsBytes);
+                var ip = new IPAddress(ipAsBytes);
+                result.Add(ip);
+            }
+
+            return result;
+        }
+    }
+}
