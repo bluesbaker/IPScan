f091a9bbbb1e8702b30f4454f41ee35c515dacb2
Константин <bluesbaker@mail.ru>
2020-07-02 23:49:35 +0300

    Add ScannerResultHeadersAttribute

---
+++ IPScan/Supports/ScannerResultHeadersAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPScan.Supports
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ScannerResultHeadersAttribute : Attribute
+    {
+        public ScannerResultHeadersAttribute(params string[] headers)
+        {
+            Headers = headers;
+        }
+
+        public string[] Headers { get; private set; }
+    }
+}
