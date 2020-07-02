83ee8bae130c911127183115534ee657347fe10d
Константин <bluesbaker@mail.ru>
2020-07-02 23:51:20 +0300

    Add loading preview

--- IPScan/Program.cs
+++ IPScan/Program.cs
@@ -16,51 +16,85 @@ namespace IPScan
     {
         static void Main(string[] args)
         {
-            string[] tmpArg = { "ipscan", "-ip", "8.8.8.8" };
             Console.WriteLine(welcomeMessage);
 
             string parametersLine = String.Empty;
             while(parametersLine.ToLower().Trim() != "q")
             {
-                ScannerWizard wizard = new ScannerWizard();
-                wizard.ScanCollection.Add(new PingScanner());
+                IPScan ipScan = new IPScan();
+                ipScan.ScanCollection.Add(new PingScanner());
 
+                Console.Write("> ");
                 parametersLine = Console.ReadLine();
+                var ipParameters = IPScanParameters.Parse(parametersLine.Split(' '), "-ip");
 
-                wizard.Init(Parameters.Parse(parametersLine.Split(' '), "-ip"));
 
-                var task = wizard.Run();
 
-                while (!task.IsCompleted)
-                {
-                    string loading = DateTime.Now.Second % 2 == 0 ? "[.'.'.'.'.]" : "['.'.'.'.']";
-                    Console.Write(loading);
-                    Thread.Sleep(1000);
-                    Console.Write("\b\b\b\b\b\b\b\b\b\b\b           \b\b\b\b\b\b\b\b\b\b\b");
-                }
-                task.Wait();
+                ipScan.Init(ipParameters);
 
-                Console.WriteLine();
+                var task = ipScan.Run();
+
+                LoadingPreview("Scanning " + ipParameters["-ip"], (() => !task.IsCompleted));
+
+                //task.Wait();
+                
                 foreach(var prop in task.Result)
                 {
                     Console.Write(prop.Value + "\t\t|");
                 }
                 Console.WriteLine();
-            }
-            
-
+            }           
             
             Console.Write("Press any key to exit...");
             Console.ReadLine();
         }
 
-        #region Private
-        static readonly string welcomeMessage =
-            $"ipscan – scanning ip-address\n______\n" +
-            "usage: 192.168.0.1-192.168.0.255\n" +
-            " help: --help\n";
+        #region Private Methods
+        private static void LoadingPreview(string title, Func<bool> predicate, int pause = 100, int dotCount = 3)
+        {
+            int dots = 0;
+
+            // -> delete last symbol at terminal
+            void WriteBackspace(int count)
+            {
+                int c = count;
+                while (c != 0)
+                {
+                    Console.Write("\b \b");
+                    c--;
+                }
+            }
+
+            // view
+            Console.Write(title);
+            while (predicate.Invoke())
+            {
+                dots++;
+                Console.Write(".");
+                Thread.Sleep(pause);
+
+                // clear dots
+                if (dots >= dotCount)
+                {
+                    WriteBackspace(dots);
+                    dots = 0;
+                }                
+            }
+
+            // clear last dots
+            WriteBackspace(dots);
+
+            // clear title
+            WriteBackspace(title.Length);
+        }
         #endregion
 
-        
+        #region Private
+        static readonly string welcomeMessage =
+            $"IPScan – scanning ip-address\n" +
+            $"----------------------------\n" +
+            "Usage:\t192.168.0.1-192.168.0.255\n" +
+            "Help:\t--help or -help\n";
+        #endregion
     }
 }
