09b011373e0c1fd22a20ace53f3a2ec91b078356
Константин <bluesbaker@mail.ru>
2020-07-03 21:43:37 +0300

    Rename ScannerArgumentAttribute to ScannerKeyAttribute

--- IPScan/Supports/ScannerKeyAttribute.cs
+++ IPScan/Supports/ScannerKeyAttribute.cs
@@ -11,23 +11,23 @@ namespace IPScan.Supports
     /// and the default value
     /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
-    public class ScannerArgumentAttribute : Attribute
+    public class ScannerKeyAttribute : Attribute
     {
         /// <summary>
-        /// Declarate an option argument
+        /// Declarate an option key
         /// </summary>
-        /// <param name="argument">For example '-a'</param>
-        /// <param name="description">Argument description</param>
+        /// <param name="key">For example '-k'</param>
+        /// <param name="description">Key description</param>
         /// <param name="defaultValue">Default value</param>
-        public ScannerArgumentAttribute(string argument, string description, object defaultValue, bool isRequired = false)
+        public ScannerKeyAttribute(string key, string description, object defaultValue, bool isRequired = false)
         {
-            Argument = argument;
+            TextKey = key;
             Description = description;
             DefaultValue = defaultValue;
             IsRequired = isRequired;
         }
 
-        public string Argument { get; private set; }
+        public string TextKey { get; private set; }
         public string Description { get; private set; }
         public object DefaultValue { get; private set; }
         public bool IsRequired { get; private set; }
