d542245b2b5adf9a2cf52da51fdc9ffce10c980d
Константин <bluesbaker@mail.ru>
2020-07-04 12:25:29 +0300

    Add Merge method
    Merge() implements merges two IPInfo object

--- IPScan/IPInfo.cs
+++ IPScan/IPInfo.cs
@@ -8,6 +8,20 @@ namespace IPScan
 {
     public class IPInfo : Dictionary<string, string>
     {
-
+        public void Merge(IPInfo ipInfo, bool isReplaced = true)
+        {
+            foreach(var field in ipInfo)
+            {
+                if (isReplaced)
+                {
+                    this[field.Key] = field.Value;
+                }
+                else
+                {
+                    var currentField = this.FirstOrDefault(f => f.Key == field.Key);
+                    this[field.Key] = currentField.Value ?? field.Value;
+                }
+            }            
+        }
     }
 }
