1addb41d57c1a4b2a24e3930607319cc5e23b72f
Константин <bluesbaker@mail.ru>
2020-07-04 12:32:47 +0300

    Refactoring and try performance

--- IPScan/Program.cs
+++ IPScan/Program.cs
@@ -17,32 +17,60 @@ namespace IPScan
         static void Main(string[] args)
         {
             Console.WriteLine(welcomeMessage);
-
             string parametersLine = String.Empty;
+
             while(parametersLine.ToLower().Trim() != "q")
             {
+                Task<IPInfo> task = null;
                 IPScan ipScan = new IPScan();
-                ipScan.ScanCollection.Add(new PingScanner());
+                ipScan.ScannerCollection.Add(new PingScanner());
 
                 Console.Write("> ");
                 parametersLine = Console.ReadLine();
-                var ipParameters = IPScanParameters.Parse(parametersLine.Split(' '), "-ip");
 
-
-
-                ipScan.Init(ipParameters);
-
-                var task = ipScan.Run();
-
-                LoadingPreview("Scanning " + ipParameters["-ip"], (() => !task.IsCompleted));
-
-                task.Wait();
-                
-                foreach(var prop in task.Result)
+                try
                 {
-                    Console.Write(prop.Value + "\t\t|");
+                    var ipParameters = IPScanParameters.Parse(parametersLine.Split(' '), "-ip");
+                    ipScan.Init(ipParameters);
+
+                    task = ipScan.Run();
+
+                    ViewLoading("Scanning " + ipParameters["-ip"], (() => !task.IsCompleted));
+
+                    task.Wait();
+
+                    foreach (var prop in task.Result)
+                    {
+                        Console.Write(prop.Value + "\t\t|");
+                    }
+
+                    Console.WriteLine();
+                }                
+                catch
+                {
+                    foreach(var exception in task.Exception.InnerExceptions)
+                    {
+                        try
+                        {
+                            throw exception;
+                        }
+                        catch (ScannerException exc)
+                        {
+                            ViewError("Scanner error", exc.InnerException.Message);
+                            // view help
+                            Console.Write("Help:");
+                            foreach (var scan in ipScan.ScannerCollection)
+                            {
+                                Console.WriteLine(scan.Help());
+                            }
+                        }
+                        catch (Exception exc)
+                        {
+                            ViewError("System error", exc.Message);
+                        }
+                    }
+                    //ViewError("System error", ex.InnerException.Message);
                 }
-                Console.WriteLine();
             }           
             
             Console.Write("Press any key to exit...");
@@ -50,7 +78,16 @@ namespace IPScan
         }
 
         #region Private Methods
-        private static void LoadingPreview(string title, Func<bool> predicate, int pause = 100, int dotCount = 3)
+        private static void ViewError(string title, string message)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(title);
+            Console.ResetColor();
+            Console.WriteLine($" {message} ");
+        }
+
+        private static void ViewLoading(string title, Func<bool> predicate, int pause = 100, int dotCount = 3)
         {
             int dots = 0;
 
@@ -72,7 +109,7 @@ namespace IPScan
                 dots++;
                 Console.Write(".");
                 Thread.Sleep(pause);
-
+                
                 // clear dots
                 if (dots >= dotCount)
                 {
