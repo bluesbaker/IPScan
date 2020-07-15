d29321f822479ac3a39dae537cac9721455fee20
Константин <bluesbaker@mail.ru>
2020-07-15 11:13:26 +0300

    Rename IPScanParametersTests to ScannerParametersTests

--- IPScan.Test/ScannerParametersTests.cs
+++ IPScan.Test/ScannerParametersTests.cs
@@ -8,7 +8,7 @@ using System.Net;
 
 namespace Tests
 {
-    public class IPScanParametersTests
+    public class ScannerParametersTests
     {
         // Parse static method
         [Test]
@@ -21,10 +21,10 @@ namespace Tests
                 ["-p"] = "27015"
             };
 
-            IPScanParameters ipScanParameters = IPScanParameters.Parse(dictionary);
-            Assert.That(ipScanParameters.Address, Is.EqualTo(IPAddress.Parse("8.8.8.8")));
-            Assert.That(ipScanParameters.Timeout, Is.EqualTo(3000));
-            Assert.That(ipScanParameters.Port, Is.EqualTo(27015));
+            ScannerParameters scannerParameters = ScannerParameters.Parse(dictionary);
+            Assert.That(scannerParameters.Address, Is.EqualTo(IPAddress.Parse("8.8.8.8")));
+            Assert.That(scannerParameters.Timeout, Is.EqualTo(3000));
+            Assert.That(scannerParameters.Port, Is.EqualTo(27015));
         }
 
         [Test]
@@ -35,10 +35,10 @@ namespace Tests
                 ["-ip"] = "8.8.8.8"
             };
 
-            IPScanParameters ipScanParameters = IPScanParameters.Parse(dictionary);
-            Assert.That(ipScanParameters.Address, Is.EqualTo(IPAddress.Parse("8.8.8.8")));
-            Assert.That(ipScanParameters.Timeout, Is.EqualTo(1000));
-            Assert.That(ipScanParameters.Port, Is.EqualTo(0));
+            ScannerParameters scannerParameters = ScannerParameters.Parse(dictionary);
+            Assert.That(scannerParameters.Address, Is.EqualTo(IPAddress.Parse("8.8.8.8")));
+            Assert.That(scannerParameters.Timeout, Is.EqualTo(1000));
+            Assert.That(scannerParameters.Port, Is.EqualTo(0));
         }
 
         [Test]
@@ -46,10 +46,10 @@ namespace Tests
         {
             var dictionary = new Dictionary<string, string>();
 
-            IPScanParameters ipScanParameters = IPScanParameters.Parse(dictionary);
-            Assert.That(ipScanParameters.Address, Is.Null);
-            Assert.That(ipScanParameters.Timeout, Is.EqualTo(1000));
-            Assert.That(ipScanParameters.Port, Is.EqualTo(0));
+            ScannerParameters scannerParameters = ScannerParameters.Parse(dictionary);
+            Assert.That(scannerParameters.Address, Is.Null);
+            Assert.That(scannerParameters.Timeout, Is.EqualTo(1000));
+            Assert.That(scannerParameters.Port, Is.EqualTo(0));
         }
 
         [Test]
@@ -60,10 +60,10 @@ namespace Tests
                 ["ip"] = "8.8.8.8"
             };
 
-            IPScanParameters ipScanParameters = IPScanParameters.Parse(dictionary);
-            Assert.That(ipScanParameters.Address, Is.Null);
-            Assert.That(ipScanParameters.Timeout, Is.EqualTo(1000));
-            Assert.That(ipScanParameters.Port, Is.EqualTo(0));
+            ScannerParameters scannerParameters = ScannerParameters.Parse(dictionary);
+            Assert.That(scannerParameters.Address, Is.Null);
+            Assert.That(scannerParameters.Timeout, Is.EqualTo(1000));
+            Assert.That(scannerParameters.Port, Is.EqualTo(0));
         }
 
         [Test]
@@ -74,7 +74,7 @@ namespace Tests
                 ["-ip"] = "8.8.8.x"
             };
 
-            var exc = Assert.Throws<IPScanException>(() => IPScanParameters.Parse(dictionary));
+            var exc = Assert.Throws<ScannerException>(() => ScannerParameters.Parse(dictionary));
             Assert.That(exc, Is.Not.Null);
         }
 
@@ -88,7 +88,7 @@ namespace Tests
                 ["-ip"] = "8.8.8.8"
             };
 
-            var isCheck = IPScanParameters.CheckingRequiredKeys(dictionary);
+            var isCheck = ScannerParameters.CheckingRequiredKeys(dictionary);
 
             Assert.That(isCheck, Is.True);
         }
@@ -101,7 +101,7 @@ namespace Tests
                 ["-t"] = "3000"
             };
 
-            var isCheck = IPScanParameters.CheckingRequiredKeys(dictionary);
+            var isCheck = ScannerParameters.CheckingRequiredKeys(dictionary);
 
             Assert.That(isCheck, Is.False);
         }
@@ -111,7 +111,7 @@ namespace Tests
         {
             var dictionary = new Dictionary<string, string>();
 
-            var isCheck = IPScanParameters.CheckingRequiredKeys(dictionary);
+            var isCheck = ScannerParameters.CheckingRequiredKeys(dictionary);
 
             Assert.That(isCheck, Is.False);
         }
@@ -121,7 +121,7 @@ namespace Tests
         [Test]
         public void GetKeySetters_CollectionRequired()
         {
-            var keySetters = IPScanParameters.GetKeySetters();
+            var keySetters = ScannerParameters.GetKeySetters();
 
             CollectionAssert.AllItemsAreInstancesOfType(keySetters, typeof(KeySetterAttribute));
         }
@@ -129,7 +129,7 @@ namespace Tests
         [Test]
         public void GetKeySetters_UniqueItemsRequired()
         {
-            var keySetters = IPScanParameters.GetKeySetters();
+            var keySetters = ScannerParameters.GetKeySetters();
 
             CollectionAssert.AllItemsAreUnique(keySetters);
         }
