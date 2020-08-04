fc38a3140d734eb6eb265694432fdc4a05007c93
Константин <bluesbaker@mail.ru>
2020-08-04 09:48:27 +0300

    Remove Supports
    Removed the Supports directory and create the IPScan.SUP project.

--- IPScan.SUP/ColorConsole.cs
+++ IPScan.SUP/ColorConsole.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace IPScan.Supports
+namespace IPScan.SUP
 {
     /// <summary>
     /// Additional text rendering tool
--- IPScan.SUP/KeySetterAttribute.cs
+++ IPScan.SUP/KeySetterAttribute.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IPScan.Supports
+namespace IPScan.SUP
 {
     /// <summary>
     /// Declaration an option key with an description
