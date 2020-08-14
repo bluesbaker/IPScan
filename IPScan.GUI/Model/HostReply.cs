df55d09ae8141c37402598a83e2e86db5103f21e
Konstantin <49731374+bluesbaker@users.noreply.github.com>
2020-08-14 20:11:03 +0300

    Add GUI Review (#14)
    * Add IPScan.GUI project
    
    * Add RelayCommand
    
    * Add NPCBase
    
    * Add HostReply model
    
    * Add IPScan.GUI project

---
+++ IPScan.GUI/Model/HostReply.cs
@@ -0,0 +1,17 @@
+﻿using IPScan.BLL;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace IPScan.GUI.Model
+{
+    public class HostReply
+    {
+        public PingReply Host { get; set; }
+        public ObservableCollection<PortReply> Ports { get; set; } = new ObservableCollection<PortReply>();
+
+    }
+}
---
+++ IPScan.SUP/NPCBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace IPScan.SUP
+{
+    /// <summary>
+    /// Implementation of INotifyPropertyChanged
+    /// </summary>
+    public class NPCBase : INotifyPropertyChanged
+    {       
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
