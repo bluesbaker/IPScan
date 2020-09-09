83c39ad4f0c5d5b622c7771bd7305cdb305c02d0
Константин <bluesbaker@mail.ru>
2020-09-09 23:59:42 +0300

    Add IDialogViewModel

---
+++ IPScan.GUI/Support/IDialogModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPScan.GUI
+{
+    /// <summary>
+    /// Special interface for the ViewControlLinker
+    /// </summary>
+    /// <typeparam name="VIEW">View/Control</typeparam>
+    public interface IDialogModel<VIEW>
+    {
+        VIEW GetView();
+        void SetView(VIEW view);
+    }
+}
