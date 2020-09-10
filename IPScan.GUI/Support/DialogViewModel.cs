da14639ea4c933660ec6e809a42ff4800867e8ab
Константин <bluesbaker@mail.ru>
2020-09-10 22:58:53 +0300

    Clear unused namespaces

--- IPScan.GUI/Support/DialogViewModel.cs
+++ IPScan.GUI/Support/DialogViewModel.cs
@@ -1,14 +1,7 @@
-﻿using IPScan.SUP;
+﻿using IPScan.GUI.Support;
+using IPScan.SUP;
 using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace IPScan.GUI
