eb6b9e0d72954e09f76c4a5f8a3eb41bbc572387
Константин <bluesbaker@mail.ru>
2020-09-15 18:18:09 +0300

    Move NPCBase from IPScan.SUP to IPScan.GUI.Support

--- IPScan.GUI/Support/NPCBase.cs
+++ IPScan.GUI/Support/NPCBase.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace IPScan.SUP
+namespace IPScan.GUI.Support
 {
     /// <summary>
     /// Implementation of INotifyPropertyChanged
