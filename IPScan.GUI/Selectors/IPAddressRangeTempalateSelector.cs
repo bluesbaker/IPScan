a320bb5e8b084e7870d12059020872ef025dc8f2
Konstantin <bluesbaker@mail.ru>
2020-09-26 21:51:40 +0300

    Add selector

---
+++ IPScan.GUI/Selectors/IPAddressRangeTempalateSelector.cs
@@ -0,0 +1,28 @@
+﻿using IPScan.GUI.Support;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace IPScan.GUI.Selectors
+{
+    public class IPAddressRangeTempalateSelector : DataTemplateSelector
+    {
+        public DataTemplate SingleAddressDataTemplate { get; set; }
+        public DataTemplate AddressRangeDataTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if(item is SingleIPAddressProvider)
+            {
+                return SingleAddressDataTemplate;
+            }
+            if(item is RangeIPAddressProvider)
+            {
+                return AddressRangeDataTemplate;
+            }
+            return SingleAddressDataTemplate;
+        }
+    }
+}
