f5a8abd6e22b7077784d28fe925f2a46e2ea8ac2
Konstantin <bluesbaker@mail.ru>
2020-09-28 13:44:08 +0300

    Add selectors
    Added address and port template selectors.

---
+++ IPScan.GUI/Selectors/PortProviderTempalateSelector.cs
@@ -0,0 +1,29 @@
+﻿using IPScan.GUI.Support;
+using IPScan.GUI.Providers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace IPScan.GUI.Selectors
+{
+    public class PortProviderTempalateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            FrameworkElement element = container as FrameworkElement;
+
+            IPortProvider provider = item as IPortProvider;
+            if(provider is SinglePortProvider)
+            {
+                return element.FindResource("SinglePortDataTemplate") as DataTemplate;
+            }
+            if(provider is RangePortProvider)
+            {
+                return element.FindResource("RangePortDataTemplate") as DataTemplate;
+            }
+            return element.FindResource("SinglePortDataTemplate") as DataTemplate;
+        }
+    }
+}
