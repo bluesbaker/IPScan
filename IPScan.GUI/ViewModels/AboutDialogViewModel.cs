eb24190a945afb79ee7fa3702169687181a49b7c
Konstantin <bluesbaker@mail.ru>
2020-09-29 15:46:32 +0300

    Add AboutDialogViewModel

---
+++ IPScan.GUI/ViewModels/AboutDialogViewModel.cs
@@ -0,0 +1,24 @@
+﻿using IPScan.GUI.Support;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IPScan.GUI.ViewModels
+{
+    public class AboutDialogViewModel : NPCBase
+    {
+        private string _title;
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+
+        private string _description;
+        public string Description
+        {
+            get => _description;
+            set => Set(ref _description, value);
+        }
+    }
+}
